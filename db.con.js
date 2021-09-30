489791cb77f293b83d1bc97995db972f2372076d
NazmulKhan-2009 <nazmulustc09@gmail.com>
2021-09-30 17:06:24 +0600

    ignore node_modules

--- db.con.js
+++ db.con.js
@@ -2,7 +2,7 @@
 const mongoose=require('mongoose')
 
 
-// const db="mongodb+srv://createZenny:createZenny20@cluster0.qnbwm.mongodb.net/hospital?retryWrites=true&w=majority"
+
 
 const localDB="mongodb://localhost:27017/hospital?retryWrites=true&w=majority"
 
