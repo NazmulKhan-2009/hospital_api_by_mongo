ea6ed69a7a542decf96d90a8a5685d001270f53c
NazmulKhan-2009 <nazmulustc09@gmail.com>
2021-09-30 18:32:23 +0600

    db env change

--- db.con.js
+++ db.con.js
@@ -2,9 +2,9 @@
 const mongoose=require('mongoose')
 
 
+const db="mongodb+srv://createZenny:createZenny20@cluster0.qnbwm.mongodb.net/hospital?retryWrites=true&w=majority"
 
-
-const localDB="mongodb://localhost:27017/hospital?retryWrites=true&w=majority"
+// const localDB="mongodb://localhost:27017/hospital?retryWrites=true&w=majority"
 
 module.exports = async function connection() {
  try {
@@ -13,7 +13,7 @@ module.exports = async function connection() {
          useCreateIndex: true,
          useUnifiedTopology: true,
      };
-     await mongoose.connect(localDB, connectionParams);
+     await mongoose.connect(db, connectionParams);
      console.log("connected to database");
  } catch (error) {
      console.log(error);
